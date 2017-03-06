edc32dac68ac334b8070af9119696e1b52a1a170
ZongAng123 <zongang15@yahoo.com>
2017-03-06 08:24:18 +0800

    Initial Commit

---
+++ ZASearchTests/ZASearchTests.m
@@ -0,0 +1,39 @@
+//
+//  ZASearchTests.m
+//  ZASearchTests
+//
+//  Created by 纵昂 on 2017/3/6.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZASearchTests : XCTestCase
+
+@end
+
+@implementation ZASearchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
